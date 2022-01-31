b290141a7db7b2b444abe89ba1f5bbbd97763e54
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-01-31 18:30:33 +0100

    added basic aimation to landing page

--- src/places/pages/Landing.js
+++ src/places/pages/Landing.js
@@ -2,27 +2,63 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Landing.css";
 import MainImg from "../../assets/Main.svg";
+import { motion } from "framer-motion";
 function Landing() {
   return (
     <>
       <div className="landing-wraper">
-        <img src={MainImg} alt="main img" className="main-img" />
-        <div>
-          <h1 className="landing-header ">Share your </h1>
-          <h1 className="landing-header " id="higlight">
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+          src={MainImg}
+          alt="main img"
+          className="main-img"
+        />
+        <div
+          initial={{ x: "100vw", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          <motion.h1
+            initial={{ x: "100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.1, duration: 0.5 }}
+            className="landing-header "
+          >
+            Share your{" "}
+          </motion.h1>
+          <motion.h1
+            initial={{ x: "100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className="landing-header "
+            id="higlight"
+          >
             favorite
-          </h1>
-          <h1 className="landing-header ">places</h1>
+          </motion.h1>
+          <motion.h1
+            initial={{ x: "100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="landing-header "
+          >
+            places
+          </motion.h1>
         </div>
       </div>
-      <div className="main-upload-wrapper">
+      <motion.div
+        initial={{ y: "1000" }}
+        animate={{ y: 0 }}
+        className="main-upload-wrapper"
+      >
         <Link
           to="/places/new"
           style={{ textDecoration: "none", color: "black" }}
         >
           <div className="main-upload-button">UPLOAD</div>
         </Link>
-      </div>
+      </motion.div>
     </>
   );
 }
